ea81e27d8d2ca66924d0324307faca6eac901eff
Isaiah <isaiahp211@nystudents.net>
2024-11-15 18:56:35 -0500

    Messed around lmao

--- main.java
+++ main.java
@@ -1,5 +1,39 @@
 public class Main {
     public static void main(String[] args) {
         //yowza it worked !!! :O
+        //make sure to have semicolon; got it;>
+        //ew camelcase yucky
+        System.out.println("TYSON VS PAUL 8:00 pm est 11/15!!!! MUST WATCH!!");
+        //my code wasnt running lmao i had main instead of Main
+        int x = 100;
+        //grr i hate having to type static evry time AHH
+        if (x == 100) {
+            System.out.println("Wowza that sure is 100!");
+        }
+        //messing around
+        //it would print 10, then 6, then true?
+        int a = 6;
+		System.out.println(a + 4);
+		System.out.println(a);
+		System.out.println(a % 2 == 0);
+        double[] arr = {1,2,3,4.5};
+        System.out.println(arr.length);
+        //this didnt work till i manually retyped it 
+        boolean[] grr = new boolean[100];
+        System.out.println(grr[1] || grr[2]);
+        int[][][] grid = new int[8][8][8];
+        int firstVal = grid[0][0][0];
+        System.out.println(firstVal);
+        while (x == 100) {
+            System.out.println("YOURE GOING TO DIE AHHAHHA");
+            //lmao
+            x = x + 1;
+        }
+        double sum = 0;
+        for (double e : arr) {
+            sum = sum + e;
+            System.out.println(sum);
+        }
+        //so smart
     }
 }
